5848d4bf629f1da52cc88c3b9d758fd9f5c2bf9e
Riccardo Capuani <riccardo.capuani@gmail.com>
2018-03-06 14:33:00 +0100

    Fix

--- src/android/BootCompletedReceiver.java
+++ src/android/BootCompletedReceiver.java
@@ -1,17 +1,37 @@
 package com.tonikorin.cordova.plugin.autostart;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import com.tonikorin.cordova.plugin.autostart.AppStarter;
- 
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+import it.peopletrust.octobike.MainActivity;
+import it.peopletrust.octobike.R;
+
 public class BootCompletedReceiver extends BroadcastReceiver {
-    
+    static final String CHANNEL_ID = "default-channel-id";
+  public static final int NOTIFICATION_ID = 0;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        
-        AppStarter appStarter = new AppStarter();
-        appStarter.run(context, intent, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, true);
+      Intent mainIntent = new Intent(context, MainActivity.class);
+      mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+      PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
+
+
+      NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
+        .setSmallIcon(R.drawable.launcher_icon)
+        .setContentTitle(context.getString(R.string.app_name))
+        .setContentText("Apri l'applicazione per permettere la localizzazione della tua bici")
+        .setStyle(new NotificationCompat.BigTextStyle()
+          .bigText("Apri l'applicazione per permettere la localizzazione della tua bici"))
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        .setContentIntent(pendingIntent)
+        .setAutoCancel(true);
+
+      NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+      notificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
 }
