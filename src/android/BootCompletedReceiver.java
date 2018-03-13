fe693fd71f5cd66f69b567c4759fadeaaaea2eb3
Riccardo Capuani <riccardo.capuani@gmail.com>
2018-03-13 14:20:03 +0100

    Fix

--- src/android/BootCompletedReceiver.java
+++ src/android/BootCompletedReceiver.java
@@ -1,18 +1,21 @@
 package com.tonikorin.cordova.plugin.autostart;
 
+import android.annotation.TargetApi;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 
 import it.peopletrust.octobike.MainActivity;
 import it.peopletrust.octobike.R;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
-  static final String CHANNEL_ID = "default-channel-id";
+  static final String CHANNEL_ID = "it.peopletrust.octobike.BikeNotification";
   public static final int NOTIFICATION_ID = 0;
 
   @Override
@@ -21,19 +24,35 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
     PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
 
+    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
     NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
       .setSmallIcon(R.drawable.ic_notification)
       .setContentTitle("Bici non connessa")
       .setContentText("Tocca per connettere la tua bici")
       .setStyle(new NotificationCompat.BigTextStyle()
-        .bigText("Apri l'applicazione per permettere la localizzazione della tua bici"))
+        .bigText("Tocca per connettere la tua bici"))
       .setPriority(NotificationCompat.PRIORITY_DEFAULT)
       .setContentIntent(pendingIntent)
       .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
       .setAutoCancel(true);
 
-    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      createChannel(notificationManager);
+      mBuilder.setChannelId(CHANNEL_ID);
+    }
+
     notificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+  }
+
+  @TargetApi(26)
+  private void createChannel(NotificationManager notificationManager) {
+    String name = "BikeBT";
+    String description = "Notifications for bike connection";
+    int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+    NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+    mChannel.setDescription(description);
+    notificationManager.createNotificationChannel(mChannel);
   }
 }
